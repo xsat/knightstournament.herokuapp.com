1b1f35e1123127897496299f6d107916d1fde666
Yuri Antonchyk <yurii.antonchyk@gmail.com>
2017-02-26 10:34:16 +0200

    Cleaner tests

---
+++ cleaner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+directories=('apps/common/cache/models/ apps/frontend/cache/views/ apps/frontend/cache/models/ apps/server/cache/models/')
+content=$'*\n!.gitignore'
+
+for dir in $directories
+do
+    rm -r $dir
+    mkdir $dir
+    file=$dir".gitignore"
+    touch $file
+    echo "$content" > $file;
+done
